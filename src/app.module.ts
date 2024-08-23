9a930b0ae380c4006ca5f4cdc71d63a628255564
Luiz Vicente <dev.vicente.contato@gmail.com>
2024-08-23 10:15:49 -0300

    chore: project configuration

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddressModule } from './modules/address/address.module';
+import typeorm from './infra/database/typeorm';
+import { FreightOperatorModule } from './modules/freight-operator/freight-operator.module';
+import { FreightQuoteModule } from './modules/freight-quote/freight-quote.module';
+import { ShopkeeperModule } from './modules/shopkeeper/shopkeeper.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+    AddressModule,
+    ShopkeeperModule,
+    FreightOperatorModule,
+    FreightQuoteModule,
+  ],
+})
+export class AppModule {}
